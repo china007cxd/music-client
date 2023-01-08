b59ef1ddcb71678096889b57d1f0399f99459ef2
china007cxd <2269360185@qq.com>
2023-01-08 22:39:59 +0800

    还原版本

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  //这里只是用来支持内网穿透的,本地使用的时候放开注释
+  /*devServer: {
+    historyApiFallback: true,
+    allowedHosts: "all",
+    host:'0.0.0.0',
+    client: {
+      webSocketURL: 'ws://0.0.0.0:6103/ws',
+    },
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    }
+  },*/
   chainWebpack: config => {
     config.plugin('define').tap(definitions => {
       Object.assign(definitions[0]['process.env'], {
